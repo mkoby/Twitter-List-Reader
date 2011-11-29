94d5b0aa1ec29535b3e1c07303e83eedb5da8c02
Michael Koby <michael@mkoby.com>
2011-11-29 14:12:41 -0600

    Initial Commit

---
+++ Twitter List Reader/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Twitter List Reader
+//
+//  Created by Michael Koby on 11/29/11.
+//  Copyright (c) 2011 Teabrick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Twitter List Reader/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Twitter List Reader
+//
+//  Created by Michael Koby on 11/29/11.
+//  Copyright (c) 2011 Teabrick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
